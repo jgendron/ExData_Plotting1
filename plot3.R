662fd79ed9195c650cd87fcd6082c2c9c99b31c7
jgendron <gerald.gendron@gmail.com>
2014-05-09 14:32:57 -0400

    Final submission of plot3 code
    Signed-off-by: jgendron <gerald.gendron@gmail.com>

--- plot3.R
+++ plot3.R
@@ -43,5 +43,6 @@ lines(datarel$DTuse, datarel$submetering2, type = "l", col="red")
 lines(datarel$DTuse, datarel$submetering3, type = "l", col="blue")
 legend("topright", lty=1,col=c("black","red","blue"),
        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
 dev.off()
 
